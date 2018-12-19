26bdb06ceefc3a2dd15fc9d3dcc35e4d9b72dda1
Andres <asiman1@gmail.com>
2018-12-19 13:48:37 -0600

    Part 1 Completed and Part 2 but the Db is not working

--- src/CheeseMVC/Models/CheeseCategory.cs
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -7,6 +7,7 @@ namespace CheeseMVC.Models
 {
     public class CheeseCategory
     {
+        public IList<Cheese> Cheeses = new List<Cheese>();
         public int ID { get; set; }
         public string Name { get; set; }
     }
